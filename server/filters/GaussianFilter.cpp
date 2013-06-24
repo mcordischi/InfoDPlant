9b745cf43c2571800b931122f0187d29ec14a14a
Martín Cordischi <tinchofm@gmail.com>
2013-06-24 00:00:10 -0300

    Playing with OpenCV

--- server/filters/GaussianFilter.cpp
+++ server/filters/GaussianFilter.cpp
@@ -5,37 +5,74 @@
 
 using namespace cv;
 
-Mat* filter(Mat* input){
-  int scale = 1;
-  int delta = 0;
-  int ddepth = CV_16S;
+Class GaussianFilter : public Filter {
+    protected:
+        int scale = 1;
+        int delta = 0
+        int ddepth = CV_16S;
+    public:
 
 
-  Mat* output = new Mat(*input);
-  GaussianBlur( *input, *output, Size(3,3), 0, 0, BORDER_DEFAULT );
+    void setParameters(int scale, int delta, int ddepth){
+        this.scale = scale ;
+        this.delta = delta ;
+        this.ddepth = ddepth;
+    }
 
-  /// Convert it to gray
-  cvtColor( *output, *output, CV_RGB2GRAY );
+    Mat* filterN(Mat* input){
 
-  /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
+        Mat* output = new Mat(*input);
+        GaussianBlur( *input, *output, Size(3,3), 0, 0, BORDER_DEFAULT );
 
-  /// Gradient X
-  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-//  Sobel( *output, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-//  convertScaleAbs( grad_x, abs_grad_x );
+        /// Convert it to gray
+        cvtColor( *output, *output, CV_RGB2GRAY );
 
-  /// Gradient Y
-  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-//  Sobel( *output , grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-//  convertScaleAbs( grad_y, abs_grad_y );
+        /// Generate grad_x and grad_y
+        Mat grad_x, grad_y;
+        Mat abs_grad_x, abs_grad_y;
 
-  /// Total Gradient (approximate)
-//  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, *output );
+        /// Gradient X
+        //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+//      Sobel( *output, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+//      convertScaleAbs( grad_x, abs_grad_x );
 
-  return output;
-}
+        /// Gradient Y
+        //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+//      Sobel( *output , grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+//      convertScaleAbs( grad_y, abs_grad_y );
+
+        /// Total Gradient (approximate)
+//      addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, *output );
+
+        return output;
+    }
+
+
+    void filter(Mat* input){
+
+        GaussianBlur( *input, *input, Size(3,3), 0, 0, BORDER_DEFAULT );
+
+        /// Convert it to gray
+        cvtColor( *input, *input, CV_RGB2GRAY );
+
+        /// Generate grad_x and grad_y
+//        Mat grad_x, grad_y;
+//        Mat abs_grad_x, abs_grad_y;
+
+        /// Gradient X
+        //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+//      Sobel( *output, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+//      convertScaleAbs( grad_x, abs_grad_x );
+
+        /// Gradient Y
+        //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+//      Sobel( *output , grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+//      convertScaleAbs( grad_y, abs_grad_y );
+
+        /// Total Gradient (approximate)
+//      addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, *output );
+    }
+};
 
 /** @function main */
 int main( int argc, char** argv )
